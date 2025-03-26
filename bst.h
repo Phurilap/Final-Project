bae0690709f2be00689f351083f709b2be58c30c
Achira1234 <achira.por@student.mahidol.edu>
2025-03-26 15:15:51 +0000

    test

--- bst.h
+++ bst.h
@@ -1,1 +1,33 @@
+#ifndef BST_H
+#define BST_H
 
+#include <iostream>
+#include "employee.h"
+
+struct TreeNode {
+    Employee* employee;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(Employee* emp):employee(emp), left(nullptr), right(nullptr)
+};
+
+class CompanyBST : public Company{
+private:
+    TreeNode* root;
+
+    void insertNode(TreeNode*& node, Employee* emp);
+    Employee* findNode(TreeNode* node, const std::string& name);
+    void displayInOrder(TreeNode* node);
+    void freeTree(TreeNode* node);
+
+public:
+    CompanyBST() : root(nullptr) 
+    ~CompanyBST() { freeTree(root); }
+
+    void addEmployee(const std::string& name, int experience, double salary);
+    Employee* getEmployee(const std::string& name);
+    void displayEmployeesInOrder();
+};
+
+#endif
