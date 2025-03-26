72c98044618c58fb5381acbba95415fcf8c850df
Phurilap <phurilap.kit@student.mahidol.edu>
2025-03-26 18:01:45 +0000

    BST updated, version 2

--- bst.h
+++ bst.h
@@ -9,25 +9,61 @@ struct TreeNode {
     TreeNode* left;
     TreeNode* right;
 
-    TreeNode(Employee* emp):employee(emp), left(nullptr), right(nullptr)
+    TreeNode(Employee* emp) : employee(emp), left(nullptr), right(nullptr) {}
 };
 
-class CompanyBST : public Company{
+class CompanyBST {
 private:
     TreeNode* root;
 
-    void insertNode(TreeNode*& node, Employee* emp);
-    Employee* findNode(TreeNode* node, const std::string& name);
-    void displayInOrder(TreeNode* node);
-    void freeTree(TreeNode* node);
+    void insertNode(TreeNode*& node, Employee* emp){
+        if(!node){
+            node = new TreeNode(emp);
+        } else if(emp->getName() < node->employee->getName()){
+            insertNode(node->left, emp);
+        } else{
+            insertNode(node->right, emp);
+        }
+    }
+
+    Employee* findNode(TreeNode* node, const std::string& name){
+        if(!node) return nullptr;
+        if(name == node->employee->getName()) return node->employee;
+        if(name < node->employee->getName()) return findNode(node->left, name);
+        return findNode(node->right, name);
+    }
+
+    void displayInOrder(TreeNode* node){
+        if(node){
+            displayInOrder(node->left);
+            node->employee->display();
+            displayInOrder(node->right);
+        }
+    }
+
+    void freeTree(TreeNode* node){
+        if(node){
+            freeTree(node->left);
+            freeTree(node->right);
+            delete node;
+        }
+    }
 
 public:
-    CompanyBST() : root(nullptr) 
+    CompanyBST() : root(nullptr) {}
     ~CompanyBST() { freeTree(root); }
 
-    void addEmployee(const std::string& name, int experience, double salary);
-    Employee* getEmployee(const std::string& name);
-    void displayEmployeesInOrder();
+    void addEmployee(Employee* emp){
+        insertNode(root, emp);
+    }
+
+    Employee* getEmployee(const std::string& name){
+        return findNode(root, name);
+    }
+
+    void displayEmployeesInOrder(){
+        displayInOrder(root);
+    }
 };
 
 #endif
